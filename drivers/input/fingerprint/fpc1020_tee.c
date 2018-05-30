a966f5ea3ba4da94e769916700441f3856c8a75e
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-29 17:24:10 -0700

    [input] fpc1020: Mark IRQ as performance critical (LG msm8996)
    In order to improve fingerprint wake performance, mark the fingerprint
    reader's IRQ as performance-critical.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/input/fingerprint/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1020_tee.c
@@ -759,7 +759,7 @@ static int fpc1020_probe(struct spi_device *spi)
 	fpc1020->wakeup_enabled = false;
 	fpc1020->clocks_enabled = false;
 	fpc1020->clocks_suspended = false;
-	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL;
 
 	/* Although wakeup feature is desirable, it is doomed that the device will be
 	 * woken up due to false interrupt caused by hw reset.
